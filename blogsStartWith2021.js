a8f969ee8606e19441666ca5b44e1deede6fd7aa
Firefly0 <agrigoroaei.ionut@gmail.com>
2022-11-22 20:14:25 +0200

    Create blogsStartWith2021.js

---
+++ blogsStartWith2021.js
@@ -0,0 +1,24 @@
+const blogsStartWith2021 = [
+    {
+        title: "Suflet îmbrățișat de Cristos!",
+        subtitle: "Itinerar pentru Postul Mare 2022",
+        dataApi: "https://www.itinerar-postulmare2022.aciasi.ro/api/posts",
+        avatar: "https://itinerar-postulmare2022.aciasi.ro/images/landingPage/img/landing-bg.jpg",
+        introPost:
+            "https://www.itinerar-postulmare2022.aciasi.ro/api/get-intro-post",
+        introImgAvatar:
+            "http://itinerar-postulmare2022.ro/images/landingPage/post_intro_img.jpg",
+        nameForStoragePurpose: "2022Post",
+    },
+    {
+        title: "Pășind cu iubire pe calea speranței",
+        subtitle: "Advent 2021",
+        dataApi: "https://www.itinerar-advent2021.aciasi.ro/api/posts",
+        avatar: "http://itinerar-advent2021.aciasi.ro/images/landingPage/post_intro_img.jpg",
+        introPost:
+            "https://www.itinerar-advent2021.aciasi.ro/api/get-intro-post",
+        introImgAvatar:
+            "http://itinerar-advent2021.aciasi.ro/images/landingPage/post_intro_img.jpg",
+        nameForStoragePurpose: "2021",
+    },
+];
